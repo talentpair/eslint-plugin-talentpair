cf2ff7972e566eafbccf9e7aaf85aaee9a285c22
Jeremy Robin <jeremy@talentpair.com>
2016-11-16 12:48:39 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+module.exports.rules = {
+  "no-template-url": context => ({
+    VariableDeclarator: (node) => {
+      console.log(node);
+      if (node.name.length < 2) {
+        context.report(node, 'Variable names should be longer than 1 character');
+      }
+    }
+    // , more interception points (see https://github.com/estree/estree)
+  })
+  // more rules
+};
